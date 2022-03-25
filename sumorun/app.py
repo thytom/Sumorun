76f0dc39cd7048d7e329877c296cf7b9ec345280
Archie Hilton <psyah6@nottingham.ac.uk>
2022-03-25 11:23:15 +0000

    Implement csv output.

--- sumorun/app.py
+++ sumorun/app.py
@@ -1,4 +1,5 @@
 from genericpath import exists, isfile
+from re import I
 from . import args
 from . import io_utils
 from . import iterator
@@ -21,20 +22,53 @@ def main():
     config_file = scheme['files']['sumo_configuration']
     attributes = scheme['attributes']
     aggressive_clean = scheme['aggressive_clean']
-    
-    print("Performing vtype injection...")
-    xml_injector.xml_inject_route_file(scheme)
 
+    print("Looking for interval statements in vtype scheme...")
+
+    interval_count = 0
+    # Find the variable in vtypes to alter
+    varray = None
+    vtype_altering = None
+    attrib_to_alter = None
+    for vtype in scheme['vtypes']:
+        for attrib in vtype:
+            if isinstance(vtype.get(attrib), list):
+                varray = vtype.get(attrib)
+                vtype_altering = vtype.get('id')
+                attrib_to_alter = attrib
+                interval_count += 1
+    
+    if interval_count == 0:
+        print("No intervals. Proceeding with single run...")
+    elif interval_count > 1:
+        print("Multiple intervals in schemefile, cannot continue.")
+    else:
+        print("Intervals found. Will run for every value and output data accordingly.")
+    
     if arguments.dry_run:
         print("Dry run completed.")
         exit(0)
 
-    print("Performing {} iterations using configuration file {}..."
-        .format(iterations, config_file))
+    run_data = []
 
-    io_utils.prepare_output_dir(aggressive_clean, summary_dir)
-    summaries = iterator.iterate(config_file, summary_dir, iterations)
-    summary.print_summaries(summaries, attributes)
+    for (i, value) in enumerate(varray):
+        out_dir = summary_dir + "_{}".format(i)
+
+        for (j,vtype) in enumerate(scheme['vtypes']):
+            if vtype.get('id') == vtype_altering:
+                scheme['vtypes'][j][attrib_to_alter] = value
+
+        print("Performing vtype injection...")
+        xml_injector.xml_inject_route_file(scheme)
+
+        print("Performing {} iterations using configuration file {}..."
+            .format(iterations, config_file))
+        
+        io_utils.prepare_output_dir(aggressive_clean, out_dir)
+        summaries = iterator.iterate(config_file, out_dir, iterations)
+        run_data.append(summaries)
+
+    summary.write_full_summary(run_data, attributes)
 
     exit(0)
 
--- sumorun/summary.py
+++ sumorun/summary.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def print_summary_report(summaries, attribute):
+def summary_report_as_str(summaries, attribute):
     """Print the report on a specific attribute."""
     arrived_counts = []
     try:
@@ -10,14 +10,20 @@ def print_summary_report(summaries, attribute):
             highestarrival = [round(float(c.attrib[attribute]), 3) for c in root if c.tag == "step"]
             arrived_counts.append(max(highestarrival))
 
-        print("{: <16}: Best {: <8}  Worst {: <8}  AVG {: <8}"
-            .format(attribute, max(arrived_counts), min(arrived_counts), round(sum(arrived_counts)/len(arrived_counts), 3)))
+        return "{},{},{},{}".format(attribute, max(arrived_counts), min(arrived_counts), round(sum(arrived_counts)/len(arrived_counts), 3))
     except KeyError:
         print("Attribute '{}' does not exist.".format(attribute))
 
-def print_summaries(summaries, attributes):
-    """Print the reports on a list of attributes."""
-    eq = "".join(["=" for i in range(0, 20)])
-    print("{}REPORT{}".format(eq, eq))
-    for attribute in attributes:
-        print_summary_report(summaries, attribute)
+# def print_summaries(run_number, summaries, attributes):
+#     """Print the reports on a list of attributes."""
+#     eq = "".join(["=" for i in range(0, 20)])
+#     print("{}REPORT{}".format(eq, eq))
+#     for attribute in attributes:
+#         print_summary_report(run_number, summaries, attribute)
+
+def write_full_summary(run_data, attributes):
+    with open("./out.csv", 'w') as out_file:
+        out_file.write("run_number,attribute,best,worst,avg\n")
+        for (i, summaries) in enumerate(run_data):
+            for attribute in attributes:
+                out_file.write("{},".format(i)+summary_report_as_str(summaries,attribute)+'\n')
